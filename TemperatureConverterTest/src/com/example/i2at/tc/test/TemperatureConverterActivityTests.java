8f5efaaa12f7e73e230a1c82bbcdc4c92c0bb2f2
courseatf <courseatf1234>
2014-11-19 14:45:28 +0900

    Implement the F to C (#9)

--- TemperatureConverterTest/src/com/example/i2at/tc/test/TemperatureConverterActivityTests.java
+++ TemperatureConverterTest/src/com/example/i2at/tc/test/TemperatureConverterActivityTests.java
@@ -120,7 +120,7 @@ public class TemperatureConverterActivityTests extends
 
     @UiThreadTest
     public void testCelsiusToFahrenheitConversion() {
-    	/* TODO 5-2: 하나의 field 에 값을 입력하면, 다른  field 에 해당 값이 실시간으로 변환되어야 함
+    	/* TODO 5-2: 하나의 field 에 값을 입력하면, 다른  field 에 해당 값이 실시간으로 변환되어야 함 */
         mCelsius.clear();
         mFahrenheit.clear();
         final double c = 100;
@@ -129,11 +129,11 @@ public class TemperatureConverterActivityTests extends
         assertEquals(c, mCelsius.getNumber());
         assertTrue(mFahrenheit.requestFocus());
         assertTrue(mFahrenheit.isFocused());
-        final double expected; // COMEPLETE
-        final double actual; // COMPLETE
+        final double expected = TemperatureConverter.celsiusToFahrenheit(c);
+        final double actual = mFahrenheit.getNumber();
         final double delta = Math.abs(expected - actual);
         assertTrue("delta=" + delta + " expected=" + expected + " actual=" + actual, delta < 0.005);
-        */
+
     	assertTrue(true);
     }
 
