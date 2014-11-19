3c6da7cf1333fbec6666ba40b0bf8d18ac75ae80
courseatf <courseatf1234>
2014-11-19 15:40:14 +0900

    Implement the F to C (#9) patchset 3

--- TemperatureConverterTest/src/com/example/i2at/tc/test/TemperatureConverterTests.java
+++ TemperatureConverterTest/src/com/example/i2at/tc/test/TemperatureConverterTests.java
@@ -2,19 +2,19 @@
  * 
  */
 package com.example.i2at.tc.test;
+import com.example.i2at.tc.TemperatureConverter;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-
-import com.example.i2at.tc.TemperatureConverter;
-
 import junit.framework.TestCase;
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * @author diego
  *
  */
+
 public class TemperatureConverterTests extends TestCase {
 
 	private static final HashMap<Double, Double> sConversionTableDouble =
@@ -29,10 +29,10 @@ public class TemperatureConverterTests extends TestCase {
 		sConversionTableDouble.put(-40.0, -40.0);
 		sConversionTableDouble.put(-273.0, -459.40);
 	}
-
 	/**
 	 * @param name
 	 */
+
 	public TemperatureConverterTests(String name) {
 		super(name);
 	}
@@ -58,6 +58,15 @@ public class TemperatureConverterTests extends TestCase {
 		/* TODO 6: 섭씨온도와 화씨온도를 서로 변환할 수 있어야 함. (기대 값의 허용오차는 0.005 로 가정)
 		 * 미리 준비 된 변환 테이블을 참조하여 작성(sConversionTableDouble)
 		 */
+		Iterator itr = sConversionTableDouble.keySet().iterator();
+		while(itr.hasNext()){
+			double c = (Double)itr.next();
+			double f = sConversionTableDouble.get(c);
+			double expectCelsius= TemperatureConverter.fahrenheitToCelsius(f);
+			//assertEquals(c, expect);
+			final double delta = Math.abs(expectCelsius - c);
+			assertTrue("delta=" + delta + " expect=" + expectCelsius + " actual=" + f, delta < 0.005);
+		}
 	}
 
 	/**
@@ -66,7 +75,18 @@ public class TemperatureConverterTests extends TestCase {
 	public void testCelsiusToFahrenheit() {
 		/* TODO 6: 섭씨온도와 화씨온도를 서로 변환할 수 있어야 함. (기대 값의 허용오차는 0.005 로 가정)
 		 * 미리 준비 된 변환 테이블을 참조하여 작성(sConversionTableDouble)
-		 */		
+		 */
+		Iterator itr = sConversionTableDouble.keySet().iterator();
+		while(itr.hasNext()){
+			double c = (Double)itr.next();
+			double f = sConversionTableDouble.get(c);
+			double expectFahrenheit = TemperatureConverter.celsiusToFahrenheit(c);
+			//assertEquals(f, expect);
+			
+			final double delta = Math.abs(expectFahrenheit - f);
+		    assertTrue("delta=" + delta + " expect=" + expectFahrenheit + " actual=" + f, delta < 0.005);
+		}
+
 	}
 	
 	public final void testExceptionForLessThanAbsoluteZeroF() {
@@ -74,7 +94,7 @@ public class TemperatureConverterTests extends TestCase {
 	}
 	
 	public final void testExceptionForLessThanAbsoluteZeroC() {
-		/* TODO 7: 값에 오류가 발생했을 때 동일한 field 에 표현되어야 함 */	
+		/* TODO 7: 값에 오류가 발생했을 때 동일한 field 에 표현되어야 함 */
 	}
 	
 	public final void testPrivateConstructor() throws SecurityException, NoSuchMethodException,
